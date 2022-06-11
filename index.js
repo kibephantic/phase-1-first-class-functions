3325ef093bed92ef92989082d91b3cec54049bd0
PatrickKibe <phantickibe01@gmail.com>
2022-06-11 23:54:51 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction (callback) {
+    console.log(callback());
+  }
+  
+
+  function returnsANamedFunction() {
+    const fn = function () {};
+    return fn;
+    } 
+    
+  function returnsAnAnonymousFunction () {
+      return function () {};
+  }
